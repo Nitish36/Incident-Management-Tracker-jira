8c4c34accedfdedc21b613a32f53572c3f19fbe3
Thanuja K <119279936+Thanujak16@users.noreply.github.com>
2025-06-09 19:15:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -85,4 +85,70 @@ def write_df():
         if os.path.exists(credentialsPath):
             os.remove(credentialsPath)
 
+def read_write():
+    # Link= https://drive.google.com/file/d/127YpIp8SNvCEzkhm2zZ6sutMAi3kX3Oy/view?usp=drive_link
+    url = "https://drive.google.com/uc?export=download&id=127YpIp8SNvCEzkhm2zZ6sutMAi3kX3Oy"
+    dataset = pd.read_csv(url)
+    dataset.columns = dataset.columns.str.strip()
+    print("Columns:", dataset.columns.tolist())
+
+    # Select the relevant columns
+    data = {
+        "Created": dataset["Created"],
+        "Jira Issue Key": dataset["Jira Issue Key"],
+        "Jira Ticket Type": dataset["Jira Ticket Type"],
+        "Description": dataset["Description"],
+        "Client ID": dataset["Client ID"],
+        "City": dataset["City"],
+        "Status": dataset["Status"],
+        "Issue Date Only": dataset["Issue Date Only"],
+        "Priority": dataset["Priority"],
+        "Assigned Person": dataset["Assigned Person"],
+        "Expected Resolved Date": dataset["Expected Resolved Date"],
+        "Resolved Date": dataset["Resolved Date"],
+        "Root Cause": dataset["Root Cause"],
+        "SLA Breached": dataset["SLA Breached"],
+        "Resolution Ageing": dataset["Resolution Ageing"],
+        "Resolution TAT": dataset["Resolution TAT"],
+        "Resolution Health": dataset["Resolution Health"],
+        "Labels": dataset["Labels"],
+        "Current Date": dataset["Current Date"],
+        "Request MY": dataset["Request MY"],
+        "Resolved MY": dataset["Resolved MY"],
+    }
+    final_data = pd.DataFrame(data)
+    final_data['Issue Date Only'] = pd.to_datetime(final_data['Issue Date Only'], errors='coerce')
+    final_data['Expected Resolved Date'] = pd.to_datetime(final_data['Expected Resolved Date'], errors='coerce')
+    final_data['Resolved Date'] = pd.to_datetime(final_data['Resolved Date'], errors='coerce')
+    final_data['Current Date'] = pd.to_datetime(final_data['Current Date'], errors='coerce')
+
+    GSHEET_NAME = 'Incident Management'
+    TAB_NAME = 'Incident'
+    gsheet_secret = os.getenv("GSHEET_SECRET_KEY")
+    if not gsheet_secret:
+        print("Google Sheets secret not found in environment.")
+        return
+
+    # Write the JSON to a temporary file
+    credentialsPath = "temp_gsheet_credentials.json"
+    with open(credentialsPath, "w") as f:
+        f.write(gsheet_secret)
+
+    try:
+        gc = gspread.service_account(filename=credentialsPath)
+        sh = gc.open(GSHEET_NAME)
+        worksheet = sh.worksheet(TAB_NAME)
+        set_with_dataframe(worksheet, df)
+        print("Data loaded successfully!! Have fun!!")
+        print(df)
+    except Exception as e:
+        print(f"Error: {e}")
+    finally:
+        if os.path.exists(credentialsPath):
+            os.remove(credentialsPath)
+
+
+
+
 write_df()
+read_write()
